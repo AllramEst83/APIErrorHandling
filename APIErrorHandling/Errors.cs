700ff199c9ea4844be45124ff28194f4b930b1a1
kay Wiberg <kaywib@hotmail.com>
2019-03-07 23:29:05 +0100

    Building error handeling

--- APIErrorHandling/Errors.cs
+++ APIErrorHandling/Errors.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using ResponseModels.ViewModels;
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace APIErrorHandling
 {
     public static class Errors
     {
+        //AddErrorsToModelState
         public static ModelStateDictionary AddErrorsToModelState(IdentityResult identityResult, ModelStateDictionary modelState)
         {
             foreach (var e in identityResult.Errors)
@@ -19,146 +21,14 @@ namespace APIErrorHandling
             return modelState;
         }
 
+        //AddErrorToModelState
         public static ModelStateDictionary AddErrorToModelState(string code, string description, ModelStateDictionary modelState)
         {
             modelState.TryAddModelError(code, description);
             return modelState;
         }
 
-        public static JwtResponse ErrorResponse(JwtResponse model)
-        {
-            JwtResponse jwtErrorResonse = new JwtResponse
-            {
-                Auth_Token = model.Auth_Token,
-                Code = model.Code,
-                Id = model.Id,
-                Description = model.Description,
-                Expires_In = model.Expires_In,
-                StatusCode = model.StatusCode
-            };
-
-            return jwtErrorResonse;
-        }
-
-        public static AddUserToRoleResponse AddUserToRoleErrorResponse(AddUserToRoleResponse model)
-        {
-            AddUserToRoleResponse AddUserTorRoleErrorResonse = new AddUserToRoleResponse
-            {
-                Error = model.Error,
-                Description = model.Description,
-                StatusCode = model.StatusCode,
-                Email = model.Email,
-                Code = model.Code
-            };
-
-            return AddUserTorRoleErrorResonse;
-        }
-
-        public static GetUserRolesResponse GetUserRolesErrorResponse(GetUserRolesResponse model)
-        {
-            GetUserRolesResponse AddUserTorRoleErrorResonse = new GetUserRolesResponse
-            {
-                Error = model.Error,
-                Description = model.Description,
-                StatusCode = model.StatusCode,
-                Code = model.Code,
-                Roles = model.Roles,
-                UserId = model.UserId,
-                Email = model.Email
-            };
-
-            return AddUserTorRoleErrorResonse;
-        }
-
-        public static DeleteRoleResponse DeleteRoleErrorResponse(DeleteRoleResponse model)
-        {
-            DeleteRoleResponse AddUserTorRoleErrorResonse = new DeleteRoleResponse
-            {
-                RoleName = model.RoleName,
-                RoleId = model.RoleId,
-                Error = model.Error,
-                Description = model.Description,
-                StatusCode = model.StatusCode,
-                Code = model.Code
-            };
-
-            return AddUserTorRoleErrorResonse;
-        }
-
-        public static DeleteUserResponse DeleteUserErrorResponse(DeleteUserResponse model)
-        {
-            DeleteUserResponse deleteUserErrorResponse = new DeleteUserResponse
-            {
-                Id = model.Id,
-                Email = model.Email,
-                Error = model.Error,
-                Description = model.Description,
-                StatusCode = model.StatusCode,
-                Code = model.Code
-            };
-
-            return deleteUserErrorResponse;
-        }
-
-        public static SignUpResponse SigInErrorResponse(SignUpResponse model)
-        {
-            SignUpResponse sigInErrorResonse = new SignUpResponse
-            {
-                Error = model.Error,
-                Description = model.Description,
-                StatusCode = model.StatusCode,
-                Email = model.Email,
-                Id = model.Id,
-                Code = model.Code
-            };
-
-            return sigInErrorResonse;
-        }
-
-        public static AddRoleResponse AddRoleErrorResponse(AddRoleResponse model)
-        {
-            AddRoleResponse addRoleErrorResonse = new AddRoleResponse
-            {
-                Role = model.Role,
-                Code = model.Code,
-                Description = model.Description,
-                Error = model.Error,
-                StatusCode = model.StatusCode
-            };
-
-            return addRoleErrorResonse;
-        }
-
-        public static AddUserToRoleResponse RemoveRoleFromUserErrorResponse(AddUserToRoleResponse model)
-        {
-            AddUserToRoleResponse addRoleErrorResonse = new AddUserToRoleResponse
-            {
-                Role = model.Role,
-                Email = model.Email,
-                UserId = model.UserId,
-                Code = model.Code,
-                Description = model.Description,
-                Error = model.Error,
-                StatusCode = model.StatusCode
-            };
-
-            return addRoleErrorResonse;
-        }
-
-        public static GetAllRolesResponse GetAllRolesErrorResponse(GetAllRolesResponse model)
-        {
-            GetAllRolesResponse addRoleErrorResonse = new GetAllRolesResponse
-            {
-                ListOfAllRoles = model.ListOfAllRoles,
-                Description = model.Description,
-                Error = model.Error,
-                StatusCode = model.StatusCode,
-                Code = model.Code
-            };
-
-            return addRoleErrorResonse;
-        }
-
+        //GetGenericErrorResponse
         public static async Task<T> GetGenericErrorResponse<T>(T model)
         {
             Type objectType = model.GetType();
@@ -176,9 +46,7 @@ namespace APIErrorHandling
 
             Type t = typeof(T);
 
-            T objectInstance = await Task.FromResult((T)Activator.CreateInstance(t, args));
-
-            return objectInstance;
+            return await Task.FromResult((T)Activator.CreateInstance(t, args));
         }
     }
 }
