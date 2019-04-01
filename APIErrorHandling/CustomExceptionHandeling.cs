ef602210dd9dccfadb7ae31eb24871aa7db41fbb
kay Wiberg <kaywib@hotmail.com>
2019-04-01 21:45:40 +0200

    Pushing Custom ExceptionHandeling

---
+++ APIErrorHandling/CustomExceptionHandeling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIErrorHandling
+{
+   public class CustomExceptionHandeling
+    {
+        public async Task<T> TryCatch<R, T>(Func<Task<T>> function) where R : Exception
+        {
+            try
+            {
+                return await function();
+            }
+            catch (R ex)
+            {
+                //Skriv till logfil här
+                //https://github.com/NLog/NLog/wiki/Tutorial#best-practices-for-logging
+
+                return await Task.FromResult((T)Activator.CreateInstance(typeof(T)));
+
+                //if (ex is ArgumentNullException)
+                //{
+
+                //}
+            }
+            catch (MissingMethodException ex)
+            {
+                return await Task.FromResult((T)Activator.CreateInstance(typeof(T)));
+            }
+            //Last Exception in the order
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception thrown: {ex.Message}");
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
